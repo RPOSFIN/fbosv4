d721a907713e2201c96ec45c9b67fe30af882fb7
RPOSFIN <theauronize@gmail.com>
2026-06-19 14:12:06 +0530

    FBOS V4 Base Import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,3 @@
 export default function LoginPage() {
-  return (
-    <div
-      style={{
-        color: "white",
-        background: "red",
-        minHeight: "100vh",
-        fontSize: "48px",
-        padding: "40px",
-      }}
-    >
-      LOGIN PAGE TEST
-    </div>
-  );
+  return <div>LOGIN BYPASSED</div>;
 }
