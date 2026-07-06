7534ea253203ed2a2338a5dc057a7efb13b40c08
RPOSFIN <theauronize@gmail.com>
2026-07-07 02:47:17 +0530

    Redesign FinanceOS dashboard entry

--- app/finance/page.tsx
+++ app/finance/page.tsx
@@ -1,35 +1,5 @@
-import Link from "next/link";
+import { FinanceDashboard } from "@/components/financeos/finance-dashboard";
 
-export default function FinanceWorkbench(){
-
-const modules = [
-["Receivables","/receivables"],
-["Payables","/payables"],
-["Cashflow","/cashflow"],
-["P&L Dashboard","/pnl-dashboard"]
-];
-
-return(
-<div className="p-8">
-
-<h1 className="text-3xl font-bold mb-6">
-Finance Workbench
-</h1>
-
-<div className="grid grid-cols-4 gap-4">
-
-{modules.map((m)=>(
-<Link
-key={m[1]}
-href={m[1]}
-className="border rounded-xl p-5 hover:border-cyan-400"
->
-{m[0]}
-</Link>
-))}
-
-</div>
-
-</div>
-);
+export default function FinanceWorkbench() {
+  return <FinanceDashboard />;
 }
